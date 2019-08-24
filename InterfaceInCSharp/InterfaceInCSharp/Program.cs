1614437941ec9d7df2b5cd4d1724636ea3577fd2
Surekhasrinivasan <surekhasrao@gmail.com>
2019-08-24 09:51:12 -0400

    declare Student class inherits Marks and Location, define interface methods

--- InterfaceInCSharp/InterfaceInCSharp/Program.cs
+++ InterfaceInCSharp/InterfaceInCSharp/Program.cs
@@ -72,5 +72,36 @@ namespace InterfaceInCSharp
         void DisplayCity();
     }
 
+    public class Student : Marks, Location
+    {
+        public int StudentID;
+        public string StudentName;
+        public int marks1, marks2;
+        private string city;
+        
+        // Defining the interface InputCity method
+        public void InputCity(string pcity)
+        {
+            city = pcity;
+        }
 
+        // Defining the interface DisplayCity method
+        public void DisplayCity()
+        {
+            Console.WriteLine("City is: " + city);
+        }
+
+        // Defining the interface DisplayMarks method 
+        public void DisplayMarks()
+        {
+            Console.WriteLine("The marks of subject 1 is " + marks1);
+            Console.WriteLine("The marks of subject 2 is " + marks2);
+        }
+
+        // Defining the interface Calculate method
+        public int Calculate()
+        {
+            return marks1 + marks2;
+        }
+    }
 }
