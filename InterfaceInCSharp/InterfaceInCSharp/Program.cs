539fd84bbfbe3e3cadd3b8f4a0b30e6d45a19c69
Surekhasrinivasan <surekhasrao@gmail.com>
2019-08-24 09:35:49 -0400

    Program to demo Multiple interfaces

--- InterfaceInCSharp/InterfaceInCSharp/Program.cs
+++ InterfaceInCSharp/InterfaceInCSharp/Program.cs
@@ -9,49 +9,51 @@ namespace InterfaceInCSharp
     // Program to demo Interface demo in csharp
     
 
-    // Defining the interface
-    public interface Marks
-    {
-        // Declaring the interface methods
-        void DisplayMarks();
-        int Calculate();
-    }
+    //// Defining the interface
+    //public interface Marks
+    //{
+    //    // Declaring the interface methods
+    //    void DisplayMarks();
+    //    int Calculate();
+    //}
     
-    public class Student : Marks
-    {
-        public int StudentID;
-        public string StudentName;
-        public int marks1, marks2;
+    //public class Student : Marks
+    //{
+    //    public int StudentID;
+    //    public string StudentName;
+    //    public int marks1, marks2;
 
-        // Defining the interface DisplayMarks method 
-        public void DisplayMarks()
-        {
-            Console.WriteLine("The marks of subject 1 is " + marks1);
-            Console.WriteLine("The marks of subject 2 is " + marks2);
-        }
+    //    // Defining the interface DisplayMarks method 
+    //    public void DisplayMarks()
+    //    {
+    //        Console.WriteLine("The marks of subject 1 is " + marks1);
+    //        Console.WriteLine("The marks of subject 2 is " + marks2);
+    //    }
 
-        // Defining the interface Calculate method
-        public int Calculate()
-        {
-            return marks1 + marks2;
-        }
-    }
+    //    // Defining the interface Calculate method
+    //    public int Calculate()
+    //    {
+    //        return marks1 + marks2;
+    //    }
+    //}
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Student stud1 = new Student();
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Student stud1 = new Student();
 
-            stud1.StudentName = "John";
-            stud1.StudentID = 1;
-            stud1.marks1 = 10;
-            stud1.marks2 = 20;
+    //        stud1.StudentName = "John";
+    //        stud1.StudentID = 1;
+    //        stud1.marks1 = 10;
+    //        stud1.marks2 = 20;
 
-            stud1.DisplayMarks();
-            Console.WriteLine("The Total marks is: " + stud1.Calculate());
+    //        stud1.DisplayMarks();
+    //        Console.WriteLine("The Total marks is: " + stud1.Calculate());
 
-            Console.Read();
-        }
-    }
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program to demo Multiple interfaces 
 }
