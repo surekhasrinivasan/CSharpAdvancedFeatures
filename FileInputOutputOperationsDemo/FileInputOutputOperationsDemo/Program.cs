94c2a683bcc96731bb4a918f0dfdd4116901f6d3
Surekhasrinivasan <surekhasrao@gmail.com>
2019-08-29 21:04:03 -0400

    DriveInfo program demo initial commit

---
+++ FileInputOutputOperationsDemo/FileInputOutputOperationsDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileInputOutputOperationsDemo
+{
+    // Program shows how to use the DriveInfo class
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
