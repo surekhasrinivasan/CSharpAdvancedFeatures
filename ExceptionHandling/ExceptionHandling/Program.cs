f850cd97ef040db4a4c332288b98f8748450bcb4
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-12 20:55:23 -0400

    Program to showcase exception handling blocks

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    // Program showcases the way to use the exception handling blocks
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
