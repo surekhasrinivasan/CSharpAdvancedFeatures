5bbc1417d21720b326c3491d591bea4ad6649161
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-13 17:30:35 -0400

    Program shows how to use the built-in exceptions

--- ExceptionHandling/ExceptionHandling/Program.cs
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -6,25 +6,31 @@ using System.Threading.Tasks;
 
 namespace ExceptionHandling
 {
-    // Program showcases the way to use the exception handling blocks
+    //// Program showcases the way to use the exception handling blocks
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int[] i = new int[2];
+
+    //        // Placing the code in the try block 
+    //        try
+    //        {
+    //            // We are placing invalid code 
+    //            i[3] = 3;
+    //        }
+    //        // Here is where we catch the exception and display the error message 
+    //        catch(Exception ex)
+    //        {
+    //            Console.WriteLine(ex.Message);
+    //        }
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program shows how to use the built-in exceptions
     class Program
     {
-        static void Main(string[] args)
-        {
-            int[] i = new int[2];
 
-            // Placing the code in the try block 
-            try
-            {
-                // We are placing invalid code 
-                i[3] = 3;
-            }
-            // Here is where we catch the exception and display the error message 
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            Console.Read();
-        }
     }
 }
