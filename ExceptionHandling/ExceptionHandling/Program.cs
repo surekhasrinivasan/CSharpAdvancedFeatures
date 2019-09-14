515e18a1eb05c6e709748c81bc40aef3b0d1bc56
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-14 12:03:41 -0400

    Program to show multiple catch blocks

--- ExceptionHandling/ExceptionHandling/Program.cs
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -28,25 +28,31 @@ namespace ExceptionHandling
     //    }
     //}
 
-    // Program shows how to use the built-in exceptions
+    //// Program shows how to use the built-in exceptions
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int[] i = new int[2];
+
+    //        // Placing the code in the try block
+    //        try
+    //        {
+    //            // placing invalid code
+    //            i[3] = 3;               
+    //        }
+    //        // Here is where we catch the exception and display the error message
+    //        catch(System.IndexOutOfRangeException ex)
+    //        {
+    //            Console.WriteLine(ex.Message);
+    //        }
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program shows the way to use multiple catch blocks 
     class Program
     {
-        static void Main(string[] args)
-        {
-            int[] i = new int[2];
 
-            // Placing the code in the try block
-            try
-            {
-                // placing invalid code
-                i[3] = 3;               
-            }
-            // Here is where we catch the exception and display the error message
-            catch(System.IndexOutOfRangeException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            Console.Read();
-        }
     }
 }
