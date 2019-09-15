2029a894cce9bb2cf897617808ad59abb21f8edc
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-15 17:37:47 -0400

    Attributes in csharp - initial commit

---
+++ AttributesInCSharp/AttributesInCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributesInCSharp
+{
+    // Program shows the way to use attributes in c#
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
