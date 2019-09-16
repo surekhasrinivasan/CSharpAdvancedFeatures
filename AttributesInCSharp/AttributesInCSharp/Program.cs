87edace82a08e3a4d18efb04ea4a799cb4eb075d
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-16 17:11:34 -0400

    Program shows another way to use attributes

--- AttributesInCSharp/AttributesInCSharp/Program.cs
+++ AttributesInCSharp/AttributesInCSharp/Program.cs
@@ -6,46 +6,52 @@ using System.Threading.Tasks;
 
 namespace AttributesInCSharp
 {
-    // Program shows the way to use attributes in c#
-    [AttributeUsage(AttributeTargets.All)]
-    public class TypeAttribute : System.Attribute
-    {
-        public readonly string Type;
-        public string Subject // Topic is a named parameter
-        {
-            get
-            {
-                return Subject;
-            }
-            set
-            {
-                Subject = value;
-            }
-        }
+    //// Program shows the way to use attributes in c#
+    //[AttributeUsage(AttributeTargets.All)]
+    //public class TypeAttribute : System.Attribute
+    //{
+    //    public readonly string Type;
+    //    public string Subject // Topic is a named parameter
+    //    {
+    //        get
+    //        {
+    //            return Subject;
+    //        }
+    //        set
+    //        {
+    //            Subject = value;
+    //        }
+    //    }
 
-        public TypeAttribute(string type)  // type is a positional parameter
-        {
-            this.Type = type;
-        }
-    }
+    //    public TypeAttribute(string type)  // type is a positional parameter
+    //    {
+    //        this.Type = type;
+    //    }
+    //}
 
-    [TypeAttribute("Student")]
-    class Person
-    {
-        public int ID;
-        public string name;
+    //[TypeAttribute("Student")]
+    //class Person
+    //{
+    //    public int ID;
+    //    public string name;
 
-        void Display()
-        {
-            Console.WriteLine("The ID of the student is " + ID);
-            Console.WriteLine("The Name of the student is " + name);
-        }
-    }
+    //    void Display()
+    //    {
+    //        Console.WriteLine("The ID of the student is " + ID);
+    //        Console.WriteLine("The Name of the student is " + name);
+    //    }
+    //}
 
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //    }
+    //}
+
+    // Program shows another way to use attributes in c#
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
+
     }
 }
