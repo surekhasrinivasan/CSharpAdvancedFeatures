12049e2962edac9b7a407912eadce6fcb6873bb7
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-17 16:25:09 -0400

    Properties in CSharp - initial commit

---
+++ PropertiesInCsharp/PropertiesInCsharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesInCsharp
+{
+    // Program below shows how to use fields in a class
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
