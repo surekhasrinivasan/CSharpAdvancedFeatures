b857724cfd729250e3832f4538d3f581fc2726d3
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-18 20:07:51 -0400

    Program to show the use of properties in a class

--- PropertiesInCsharp/PropertiesInCsharp/Program.cs
+++ PropertiesInCsharp/PropertiesInCsharp/Program.cs
@@ -6,36 +6,42 @@ using System.Threading.Tasks;
 
 namespace PropertiesInCsharp
 {
-    // Program below shows how to use fields in a class
+    //// Program below shows how to use fields in a class
 
-    class Student
-    {
-        private int ID;
-        private string name;
+    //class Student
+    //{
+    //    private int ID;
+    //    private string name;
 
-        // Method used to input the value of ID and name 
-        public void Input(int pid, string pname)
-        {
-            ID = pid;
-            name = pname;
-        }
+    //    // Method used to input the value of ID and name 
+    //    public void Input(int pid, string pname)
+    //    {
+    //        ID = pid;
+    //        name = pname;
+    //    }
 
-        // Method used to display the value of ID and name 
-        public void Display()
-        {
-            Console.WriteLine("The ID of the student is " + ID);
-            Console.WriteLine("The name of the student is " + name);
-        }
-    }
+    //    // Method used to display the value of ID and name 
+    //    public void Display()
+    //    {
+    //        Console.WriteLine("The ID of the student is " + ID);
+    //        Console.WriteLine("The name of the student is " + name);
+    //    }
+    //}
 
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Student s1 = new Student();
+    //        s1.Input(10, "John");
+    //        s1.Display();
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program shows how to use properties in a class
     class Program
     {
-        static void Main(string[] args)
-        {
-            Student s1 = new Student();
-            s1.Input(10, "John");
-            s1.Display();
-            Console.Read();
-        }
+
     }
 }
