0f73c758fe15a0e8afa7d1e611987b58e0509ef1
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-21 12:24:39 -0400

    Program to show the way to use Delegate

---
+++ DelegatesInCsharp/DelegatesInCsharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesInCsharp
+{
+    // Program is used to show the way to use a delegate
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
