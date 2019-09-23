49e40f3be696bb530c63d49597521bf2e402fb66
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-23 17:35:36 -0400

    Program shows the way to use a delegate with parameters

--- DelegatesInCsharp/DelegatesInCsharp/Program.cs
+++ DelegatesInCsharp/DelegatesInCsharp/Program.cs
@@ -29,39 +29,45 @@ namespace DelegatesInCsharp
     //    }
     //}
 
-    // Program shows how to use Delegate with multiple methods 
+    //// Program shows how to use Delegate with multiple methods 
+    //class Program
+    //{
+    //    // This is the method which will be pointed to by the delegate function 
+    //    public static void DisplayA()
+    //    {
+    //        Console.WriteLine("Hello World!!");
+    //    }
+
+    //    public static void DisplayB()
+    //    {
+    //        Console.WriteLine("Hello World again");
+    //    }
+
+    //    // Defining the delegate
+    //    public delegate void Del();
+
+    //    // the main function
+    //    static void Main(string[] args)
+    //    {
+    //        // Assigning the method to the delegate
+    //        Del handler = DisplayA;
+
+    //        // Calling the method via the delegate
+    //        handler();
+
+    //        // Assigning the method to the delegate 
+    //        handler = DisplayB;
+
+    //        // Calling the method via the delegate
+    //        handler();
+
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program shows the way to use a delegate with parameters 
     class Program
     {
-        // This is the method which will be pointed to by the delegate function 
-        public static void DisplayA()
-        {
-            Console.WriteLine("Hello World!!");
-        }
-
-        public static void DisplayB()
-        {
-            Console.WriteLine("Hello World again");
-        }
-
-        // Defining the delegate
-        public delegate void Del();
-
-        // the main function
-        static void Main(string[] args)
-        {
-            // Assigning the method to the delegate
-            Del handler = DisplayA;
-
-            // Calling the method via the delegate
-            handler();
-
-            // Assigning the method to the delegate 
-            handler = DisplayB;
-
-            // Calling the method via the delegate
-            handler();
-
-            Console.Read();
-        }
+        
     }
 }
