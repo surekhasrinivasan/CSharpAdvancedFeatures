26943e5de20b4d009e3eb398bf4ebf1eb078c746
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-25 17:49:00 -0400

    Program shows how to use Type to get information on a class - initial commit

---
+++ ReflectionInCsharp/ReflectionInCsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionInCsharp
+{
+    // Reflection is the ability in csharp to get information of a particular piece of code at runtime. 
+
+    // Program shows how to use Type to get information on a class
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
