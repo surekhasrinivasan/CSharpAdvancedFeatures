8816adbdc24855fe7a35cd434fd2f8a259ee1567
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-25 17:50:31 -0400

    declare Student class and define the members

--- ReflectionInCsharp/ReflectionInCsharp/Program.cs
+++ ReflectionInCsharp/ReflectionInCsharp/Program.cs
@@ -9,6 +9,13 @@ namespace ReflectionInCsharp
     // Reflection is the ability in csharp to get information of a particular piece of code at runtime. 
 
     // Program shows how to use Type to get information on a class
+    class Student
+    {
+        // Defining the members
+        public int id;
+        public string name;
+    }
+
     class Program
     {
         static void Main(string[] args)
