272907a441c209ab884f9cbdd0a66be2ddb54b05
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-26 17:35:48 -0400

    Program to show the way to use MethodInfo - initial commit

--- ReflectionInCsharp/ReflectionInCsharp/Program.cs
+++ ReflectionInCsharp/ReflectionInCsharp/Program.cs
@@ -8,32 +8,38 @@ namespace ReflectionInCsharp
 {
     // Reflection is the ability in csharp to get information of a particular piece of code at runtime. 
 
-    // Program shows how to use Type to get information on a class
-    class Student
-    {
-        // Defining the members
-        public int id;
-        public string name;
+    //// Program shows how to use Type to get information on a class
+    //class Student
+    //{
+    //    // Defining the members
+    //    public int id;
+    //    public string name;
 
-        public void Display()
-        {
-            Console.WriteLine("The ID is " + id);
-            Console.WriteLine("The name is " + name);
-        }
-    }
+    //    public void Display()
+    //    {
+    //        Console.WriteLine("The ID is " + id);
+    //        Console.WriteLine("The name is " + name);
+    //    }
+    //}
 
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Student stud1 = new Student();
+
+    //        // Trying to get the type of object 
+    //        Type myTypeObj = stud1.GetType();
+
+    //        Console.WriteLine("The object is of Type " + myTypeObj);
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program shows the way to use MethodInfo
     class Program
     {
-        static void Main(string[] args)
-        {
-            Student stud1 = new Student();
 
-            // Trying to get the type of object 
-            Type myTypeObj = stud1.GetType();
-
-            Console.WriteLine("The object is of Type " + myTypeObj);
-            Console.Read();
-        }
     }
 }
 
