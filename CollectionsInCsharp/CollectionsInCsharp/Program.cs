9aefe13a56ed0cbeb5a69ff4e40bff27ee65b064
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-27 19:44:10 -0400

    Program to show the way to use array lists

---
+++ CollectionsInCsharp/CollectionsInCsharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsInCsharp
+{
+    // Program to show the way to use array lists
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
