bd67c603848f802ea1788880e91247225dc9745d
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-27 19:46:09 -0400

    defining the arrayList

--- CollectionsInCsharp/CollectionsInCsharp/Program.cs
+++ CollectionsInCsharp/CollectionsInCsharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace CollectionsInCsharp
     {
         static void Main(string[] args)
         {
+            // Defining the ArrayList 
+            ArrayList ar = new ArrayList();
         }
     }
 }
