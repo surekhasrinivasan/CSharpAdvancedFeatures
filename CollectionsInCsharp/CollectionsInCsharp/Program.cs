e752bf7f37bd2cad62283d67ed466f59dd508104
Surekhasrinivasan <surekhasrao@gmail.com>
2019-09-28 11:45:52 -0400

    Program to show the way to use array lists of strings - initial commit

--- CollectionsInCsharp/CollectionsInCsharp/Program.cs
+++ CollectionsInCsharp/CollectionsInCsharp/Program.cs
@@ -7,24 +7,30 @@ using System.Threading.Tasks;
 
 namespace CollectionsInCsharp
 {
-    // Program to show the way to use array lists
+    //// Program to show the way to use array lists
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        // Defining the ArrayList 
+    //        ArrayList ar = new ArrayList();
+
+    //        // Adding elements to the array list 
+    //        ar.Add(1);
+    //        ar.Add(2);
+    //        ar.Add(3);
+
+    //        // Displaying the elements of the array 
+    //        Console.WriteLine("The first element of the array is " + ar[0]);
+    //        Console.WriteLine("The second element of the array is " + ar[1]);
+    //        Console.WriteLine("The third element of the array is " + ar[2]);
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program shows the way to use array lists of strings 
     class Program
     {
-        static void Main(string[] args)
-        {
-            // Defining the ArrayList 
-            ArrayList ar = new ArrayList();
 
-            // Adding elements to the array list 
-            ar.Add(1);
-            ar.Add(2);
-            ar.Add(3);
-
-            // Displaying the elements of the array 
-            Console.WriteLine("The first element of the array is " + ar[0]);
-            Console.WriteLine("The second element of the array is " + ar[1]);
-            Console.WriteLine("The third element of the array is " + ar[2]);
-            Console.Read();
-        }
     }
 }
