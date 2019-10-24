b675d0d0c9ea01e5ead7c19249f5397038ff61dd
Surekhasrinivasan <surekhasrao@gmail.com>
2019-10-24 17:45:18 -0400

    defining the SortedList

--- CollectionsInCsharp/CollectionsInCsharp/Program.cs
+++ CollectionsInCsharp/CollectionsInCsharp/Program.cs
@@ -615,6 +615,10 @@ namespace CollectionsInCsharp
     // Program to show how to use the ContainsKey function
     class Program
     {
-
+        static void Main(string[] args)
+        {
+            // Defining the SortedList
+            SortedList ar = new SortedList();
+        }
     }
 }
