8e68953b4a44e0b28d266a909d42946dd742cce6
Surekhasrinivasan <surekhasrao@gmail.com>
2019-10-29 16:27:46 -0400

    adding elements to the SortedList

--- CollectionsInCsharp/CollectionsInCsharp/Program.cs
+++ CollectionsInCsharp/CollectionsInCsharp/Program.cs
@@ -713,6 +713,11 @@ namespace CollectionsInCsharp
         {
             // Defining the SortedList
             SortedList ar = new SortedList();
+
+            // Adding the elements to the SortedList
+            ar.Add(1, "One");
+            ar.Add(2, "Two");
+            ar.Add(3, "Three");
         }
     }
 }
