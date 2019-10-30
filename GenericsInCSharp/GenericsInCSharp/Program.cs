0d8f9853f03f200bca2efe143cdda94b30f7dc7f
Surekhasrinivasan <surekhasrao@gmail.com>
2019-10-30 17:50:34 -0400

    Generics- Program showcases the way to define multiple methods that achieve the same purpose

---
+++ GenericsInCSharp/GenericsInCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsInCSharp
+{
+    // Program showcases the way to define multiple methods that achieve the same purpose
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
