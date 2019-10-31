31d40d74425111c7a223d507c9ce8d4740345762
Surekhasrinivasan <surekhasrao@gmail.com>
2019-10-31 17:00:32 -0400

    Program shows how to use the generic data type

--- GenericsInCSharp/GenericsInCSharp/Program.cs
+++ GenericsInCSharp/GenericsInCSharp/Program.cs
@@ -6,26 +6,32 @@ using System.Threading.Tasks;
 
 namespace GenericsInCSharp
 {
-    // Program showcases the way to define multiple methods that achieve the same purpose
+    //// Program showcases the way to define multiple methods that achieve the same purpose
+    //class Program
+    //{
+    //    // Display method for Integers
+    //    public static void Display(int i)
+    //    {
+    //        Console.WriteLine("The value is " + i);
+    //    }
+
+    //    // Display method for double numbers
+    //    public static void Display(double i)
+    //    {
+    //        Console.WriteLine("The value is " + i);
+    //    }
+
+    //    static void Main(string[] args)
+    //    {
+    //        Display(1);
+    //        Display(1.1);
+    //        Console.Read();
+    //    }
+    //}
+
+    // Program shows how to use the generic data type
     class Program
     {
-        // Display method for Integers
-        public static void Display(int i)
-        {
-            Console.WriteLine("The value is " + i);
-        }
 
-        // Display method for double numbers
-        public static void Display(double i)
-        {
-            Console.WriteLine("The value is " + i);
-        }
-
-        static void Main(string[] args)
-        {
-            Display(1);
-            Display(1.1);
-            Console.Read();
-        }
     }
 }
