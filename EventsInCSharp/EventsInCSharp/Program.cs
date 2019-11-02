7d08676b5ef8a48a8d2bc6628688f7e0ea336065
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-02 19:18:01 -0400

    declare EventClass

--- EventsInCSharp/EventsInCSharp/Program.cs
+++ EventsInCSharp/EventsInCSharp/Program.cs
@@ -8,6 +8,19 @@ namespace EventsInCSharp
 {
     // Program to showcase the way to work with Events
 
-    public delegate string newDel(string str);  
+    public delegate string newDel(string str);
 
+    class EventClass
+    {
+        event newDel MyEvent;
+        public void TriggerEvent()
+        {
+            this.MyEvent += new newDel(this.mymethod);
+        }
+
+        public string mymethod(string var)
+        {
+            return "Hello" + var;
+        }
+    }
 }
