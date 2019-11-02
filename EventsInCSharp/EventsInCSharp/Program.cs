2dd9c14cf2dab78921a6a1462d12d9e4a52d8af0
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-02 19:14:00 -0400

    Program showcase to work with Events

---
+++ EventsInCSharp/EventsInCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsInCSharp
+{
+    // Program to showcase the way to work with Events
+}
