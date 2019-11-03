f1c4f5b7f616ba206e3667e49daa8eb91f54aa70
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-03 18:25:13 -0500

    method to be called by the thread

--- MultithreadingInCSharp/MultithreadingInCSharp/Program.cs
+++ MultithreadingInCSharp/MultithreadingInCSharp/Program.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 namespace MultithreadingInCSharp
 {
     // Program to show how to create a thread 
-
+    class Program
+    {
+        // This method will be called by the thread
+        public static void ThreadModule()
+        {
+            Console.WriteLine("The thread is starting");
+        }
+    }
     
 }
