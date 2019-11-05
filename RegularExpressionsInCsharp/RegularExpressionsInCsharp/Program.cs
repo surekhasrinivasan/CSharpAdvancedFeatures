7f4874b72f3f7bd2647d4b685e45de5f8becfd2a
Surekhasrinivasan <surekhasrao@gmail.com>
2019-11-05 16:19:27 -0500

    Another Example Program to showcase the way to use regular expressions

--- RegularExpressionsInCsharp/RegularExpressionsInCsharp/Program.cs
+++ RegularExpressionsInCsharp/RegularExpressionsInCsharp/Program.cs
@@ -7,25 +7,31 @@ using System.Threading.Tasks;
 
 namespace RegularExpressionsInCsharp
 {  
-    // Program to showcase the way to use regular expressions 
+    //// Program to showcase the way to use regular expressions 
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        string src = "Hello World!";
+    //        string pattern = @"Hello";
+
+    //        Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+
+    //        MatchCollection matches = rgx.Matches(src);
+    //        if(matches.Count > 0)
+    //        {
+    //            foreach(Match m in matches)
+    //            {
+    //                Console.WriteLine(m);
+    //            }
+    //        }
+    //        Console.Read();
+    //    }
+    //}
+
+    // Another Program to showcase how to use regular expressions
     class Program
     {
-        static void Main(string[] args)
-        {
-            string src = "Hello World!";
-            string pattern = @"Hello";
 
-            Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
-
-            MatchCollection matches = rgx.Matches(src);
-            if(matches.Count > 0)
-            {
-                foreach(Match m in matches)
-                {
-                    Console.WriteLine(m);
-                }
-            }
-            Console.Read();
-        }
     }
 }
